755f1f093759b0a69ae432d621b5224ec074383a
Zhi Ling <156146496+zlnortheastern@users.noreply.github.com>
2024-05-27 11:54:52 +0800

    Finished lab2 exercises and implement navigation styling

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -1,23 +1,23 @@
 import Dashboard from "./Dashboard";
+import "./styles.css";
 import KanbasNavigation from "./Navigation";
 import { Routes, Route, Navigate } from "react-router";
 import Courses from "./Courses";
 export default function Kanbas() {
   return (
-    <div id="wd-kanbas">
-      <table>
-        <tr>
-          <td valign="top">
-            <KanbasNavigation />
-          </td>
-          <td valign="top">
-            <Routes>
-              <Route path="/" element={<Navigate to="Dashboard" />} />
-              <Route path="Dashboard" element={<Dashboard />} />
-              <Route path="Courses/:id/*" element={<Courses />} />
-            </Routes>
-          </td>
-        </tr>
-      </table>
-    </div>
-);}
+    <div id="wd-kanbas"  className="h-100">
+      <div className="d-flex h-100">
+        <div className="d-none d-md-block bg-black">
+          <KanbasNavigation />
+        </div>
+        <div className="flex-fill p-4">
+          <Routes>
+            <Route path="/" element={<Navigate to="Dashboard" />} />
+            <Route path="Dashboard" element={<Dashboard />} />
+            <Route path="Courses/:id/*" element={<Courses />} />
+          </Routes>
+        </div>
+      </div>
+    </div >
+  );
+}
