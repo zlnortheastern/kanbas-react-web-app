011f7b0a5e5759953fa073a2fb1f80079226cf18
Zhi Ling <156146496+zlnortheastern@users.noreply.github.com>
2024-05-27 12:16:21 +0800

    Implement dashboard and course navigation styling

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -1,121 +1,148 @@
 export default function Dashboard() {
   return (
-    <div>
+    <div id="wd-dashboard">
       <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
-      <h2 id="wd-dashboard-published">Published Courses (8)</h2> <hr />
-      <div id="wd-dashboard-courses">
-        <div className="wd-dashboard-course">
-          <img src="/images/webdev.jfif" width={200} />
-          <div>
-            <a className="wd-dashboard-course-link"
-              href="#/Kanbas/Courses/Home">
-              CS5610 Web Development
-            </a>
-            <p className="wd-dashboard-course-title">
-              Web Development 2024 Summer
-            </p>
-            <a href="#/Kanbas/Courses/Home"> Go </a>
+      <h2 id="wd-dashboard-published" >Published Courses (8)</h2> <hr />
+      <div id="wd-dashboard-courses" className="row">
+        <div className="row row-cols-1 row-cols-md-5 g-4">
+          <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+            <div className="card">
+              <img src="/images/webdev.jfif" />
+              <div className="card-body">
+                <a className="wd-dashboard-course-link"
+                  href="#/Kanbas/Courses/Home"
+                  style={{ textDecoration: "none", color: "navy", fontWeight: "bold" }}>
+                  CS5610 Web Development
+                </a>
+                <p className="wd-dashboard-course-title card-text">
+                  Web Development 2024 Summer
+                </p>
+                <a href="#/Kanbas/Courses/Home" className="btn btn-primary"> Go </a>
+              </div>
+            </div>
           </div>
-        </div>
 
-        <div className="wd-dashboard-course">
-          <img src="/images/java.png" width={200} />
-          <div>
-            <a className="wd-dashboard-course-link"
-              href="#/Kanbas/Courses/Home">
-              CS1111 Java
-            </a>
-            <p className="wd-dashboard-course-title">
-              Java fundamental development
-            </p>
-            <a href="#/Kanbas/Courses/Home"> Go </a>
-          </div>
-        </div>
 
-        <div className="wd-dashboard-course">
-          <img src="/images/python.png" width={200} />
-          <div>
-            <a className="wd-dashboard-course-link"
-              href="#/Kanbas/Courses/Home">
-              CS2222 Python
-            </a>
-            <p className="wd-dashboard-course-title">
-              Python fundamental development
-            </p>
-            <a href="#/Kanbas/Courses/Home"> Go </a>
+          <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+            <div className="card">
+              <img src="/images/java.png" />
+              <div className="card-body">
+                <a className="wd-dashboard-course-link"
+                  href="#/Kanbas/Courses/Home"
+                  style={{ textDecoration: "none", color: "navy", fontWeight: "bold" }}>
+                  CS1111 Java
+                </a>
+                <p className="wd-dashboard-course-title card-text">
+                  Java fundamental development
+                </p>
+                <a href="#/Kanbas/Courses/Home" className="btn btn-primary"> Go </a>
+              </div>
+            </div>
           </div>
-        </div>
 
-        <div className="wd-dashboard-course">
-          <img src="/images/mysql.png" width={200} />
-          <div>
-            <a className="wd-dashboard-course-link"
-              href="#/Kanbas/Courses/Home">
-              CS3333 MySQL
-            </a>
-            <p className="wd-dashboard-course-title">
-              MySQL database management system
-            </p>
-            <a href="#/Kanbas/Courses/Home"> Go </a>
+          <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+            <div className="card">
+              <img src="/images/python.png" />
+              <div className="card-body">
+                <a className="wd-dashboard-course-link"
+                  href="#/Kanbas/Courses/Home"
+                  style={{ textDecoration: "none", color: "navy", fontWeight: "bold" }}>
+                  CS2222 Python
+                </a>
+                <p className="wd-dashboard-course-title card-text">
+                  Python fundamental development
+                </p>
+                <a href="#/Kanbas/Courses/Home" className="btn btn-primary"> Go </a>
+              </div>
+            </div>
           </div>
-        </div>
 
-        <div className="wd-dashboard-course">
-          <img src="/images/nodejs.png" width={200} />
-          <div>
-            <a className="wd-dashboard-course-link"
-              href="#/Kanbas/Courses/Home">
-              CS4444 NodeJS
-            </a>
-            <p className="wd-dashboard-course-title">
-              JavaSctipt framework development
-            </p>
-            <a href="#/Kanbas/Courses/Home"> Go </a>
+          <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+            <div className="card">
+              <img src="/images/mysql.png" />
+              <div className="card-body">
+                <a className="wd-dashboard-course-link"
+                  href="#/Kanbas/Courses/Home"
+                  style={{ textDecoration: "none", color: "navy", fontWeight: "bold" }}>
+                  CS3333 MySQL
+                </a>
+                <p className="wd-dashboard-course-title card-text">
+                  MySQL database management system
+                </p>
+                <a href="#/Kanbas/Courses/Home" className="btn btn-primary"> Go </a>
+              </div>
+            </div>
           </div>
-        </div>
 
-        <div className="wd-dashboard-course">
-          <img src="/images/vue.png" width={200} />
-          <div>
-            <a className="wd-dashboard-course-link"
-              href="#/Kanbas/Courses/Home">
-              CS5555 VUE
-            </a>
-            <p className="wd-dashboard-course-title">
-              Progressive Web Development
-            </p>
-            <a href="#/Kanbas/Courses/Home"> Go </a>
+          <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+            <div className="card">
+              <img src="/images/nodejs.png" />
+              <div className="card-body">
+                <a className="wd-dashboard-course-link"
+                  href="#/Kanbas/Courses/Home"
+                  style={{ textDecoration: "none", color: "navy", fontWeight: "bold" }}>
+                  CS4444 NodeJS
+                </a>
+                <p className="wd-dashboard-course-title card-text">
+                  JavaSctipt framework development
+                </p>
+                <a href="#/Kanbas/Courses/Home" className="btn btn-primary"> Go </a>
+              </div>
+            </div>
           </div>
-        </div>
 
-        <div className="wd-dashboard-course">
-          <img src="/images/unity.webp" width={200} />
-          <div>
-            <a className="wd-dashboard-course-link"
-              href="#/Kanbas/Courses/Home">
-              CS6666 Unity3D
-            </a>
-            <p className="wd-dashboard-course-title">
-              Game development
-            </p>
-            <a href="#/Kanbas/Courses/Home"> Go </a>
+          <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+            <div className="card">
+              <img src="/images/vue.png" />
+              <div className="card-body">
+                <a className="wd-dashboard-course-link"
+                  href="#/Kanbas/Courses/Home"
+                  style={{ textDecoration: "none", color: "navy", fontWeight: "bold" }}>
+                  CS5555 VUE
+                </a>
+                <p className="wd-dashboard-course-title card-text">
+                  Progressive Web Development
+                </p>
+                <a href="#/Kanbas/Courses/Home" className="btn btn-primary"> Go </a>
+              </div>
+            </div>
           </div>
-        </div>
 
-        <div className="wd-dashboard-course">
-          <img src="/images/chatgpt.webp" width={200} />
-          <div>
-            <a className="wd-dashboard-course-link"
-              href="#/Kanbas/Courses/Home">
-              CS7777 Artificial Intelligence
-            </a>
-            <p className="wd-dashboard-course-title">
-              LLM application optimization
-            </p>
-            <a href="#/Kanbas/Courses/Home"> Go </a>
+          <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+            <div className="card">
+              <img src="/images/unity.webp" />
+              <div className="card-body">
+                <a className="wd-dashboard-course-link"
+                  href="#/Kanbas/Courses/Home"
+                  style={{ textDecoration: "none", color: "navy", fontWeight: "bold" }}>
+                  CS6666 Unity3D
+                </a>
+                <p className="wd-dashboard-course-title card-text">
+                  Game development
+                </p>
+                <a href="#/Kanbas/Courses/Home" className="btn btn-primary"> Go </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+            <div className="card">
+              <img src="/images/chatgpt.webp" />
+              <div className="card-body">
+                <a className="wd-dashboard-course-link"
+                  href="#/Kanbas/Courses/Home"
+                  style={{ textDecoration: "none", color: "navy", fontWeight: "bold" }}>
+                  CS7777 Artificial Intelligence
+                </a>
+                <p className="wd-dashboard-course-title card-text">
+                  LLM application optimization
+                </p>
+                <a href="#/Kanbas/Courses/Home" className="btn btn-primary"> Go </a>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div >
+      </div >
+    </div>
   );
 }
