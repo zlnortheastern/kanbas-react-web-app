731c4444a92ea2e36509d8c922abe6d7e527f2c1
Zhi Ling <156146496+zlnortheastern@users.noreply.github.com>
2024-06-06 10:58:32 +0800

    Implement lab3 practice

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,20 +1,41 @@
+import Add from "./Add";
+import AddPathParameters from "./AddPathParameters";
 import AddingAndRemovingToFromArrays from "./AddingAndRemovingToFromArrays";
 import ArrayIndexAndLength from "./ArrayIndexAndLength";
 import ArrowFunctions from "./ArrowFunctions";
 import BooleanVariables from "./BooleanVariables";
+import Classes from "./Classes";
 import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
 import ConditionalOutputInline from "./ConditionalOutputInline";
+import Destructing from "./Destructing";
+import DestructingImports from "./DestructingImports";
+import FilterFunction from "./FilterFunction";
+import FindFunction from "./FindFunction";
+import FindIndex from "./FindIndex";
+import ForLoops from "./ForLoops";
+import FunctionDestructing from "./FunctionDestructing";
+import Highlight from "./Highlight";
+import House from "./House";
 import IfElse from "./IfElse";
 import ImpliedReturn from "./ImpliedReturn";
+import JsonStringify from "./JsonStringify";
 import LegacyFunctions from "./LegacyFunctions";
+import MapFunction from "./MapFunction";
+import PathParameters from "./PathParameters";
 import SimpleArrays from "./SimpleArrays";
+import Spreading from "./Spreading";
+import Square from "./Square";
+import Styles from "./Styles";
 import TemplateLiterals from "./TemplateLiterals";
 import TernaryOperator from "./TernaryOperator";
+import TodoItem from "./TodoItem";
+import TodoList from "./TodoList";
 import VariablesAndConstants from "./VariablesAndConstants";
 export default function Lab3() {
   return (
     <div id="wd-lab3" className="container-fluid">
       <h3>Lab 3</h3>
+      <h3>JavaScript</h3>
       <VariablesAndConstants />
       <BooleanVariables />
       <IfElse />
@@ -28,6 +49,32 @@ export default function Lab3() {
       <SimpleArrays />
       <ArrayIndexAndLength />
       <AddingAndRemovingToFromArrays />
+      <ForLoops />
+      <MapFunction />
+      <FindFunction />
+      <FindIndex />
+      <FilterFunction />
+      <JsonStringify />
+      <House />
+      <TodoItem />
+      <TodoList />
+      <Spreading />
+      <Destructing />
+      <FunctionDestructing />
+      <DestructingImports />
+      <Classes />
+      <Styles />
+      <Add a={3} b={4} />
+      <h4>Square of 4</h4>
+      <Square>4</Square>
+      <h4>Highlight</h4>
+      <Highlight>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione eaque illo minus cum, saepe totam
+        vel nihil repellat nemo explicabo excepturi consectetur. Modi omnis minus sequi maiores, provident voluptates.
+      </Highlight>
+      <PathParameters />
+      <AddPathParameters />
+      <hr />
     </div>
   );
 }
