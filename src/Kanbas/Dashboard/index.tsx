5d5ce38bfdbac2135e50eae1279d317911c5de68
Zhi Ling <156146496+zlnortheastern@users.noreply.github.com>
2024-06-25 10:45:10 +0800

    Implement dashboard course operations

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -1,10 +1,55 @@
 import { Link } from "react-router-dom";
 import * as db from "../Database";
+import { useState } from "react";
 export default function Dashboard() {
-  const courses = db.courses;
+  const [courses, setCourses] = useState(db.courses);
+  const [course, setCourse] = useState<any>({
+    _id: "0", name: "New Course", number: "New Number",
+    startDate: "2023-09-10", endDate: "2023-12-15",
+    image: "/images/reactjs.jpg", description: "New Description"
+  });
+
+  const addNewCourse = () => {
+    const newCourse = {
+      ...course,
+      _id: new Date().getTime().toString()
+    };
+    console.log(newCourse);
+    setCourses([...courses, { ...course, ...newCourse }]);
+  };
+  const deleteCourse = (courseId: string) => {
+    setCourses(courses.filter((course) => course._id !== courseId));
+  };
+  const updateCourse = () => {
+    setCourses(
+      courses.map((c) => {
+        if (c._id === course._id) {
+          return course;
+        } else {
+          return c;
+        }
+      })
+    );
+  };
+
+
   return (
     <div id="wd-dashboard">
       <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
+      <h5>New Course
+        <button className="btn btn-primary float-end"
+          id="wd-add-new-course-click"
+          onClick={addNewCourse} > Add </button>
+        <button className="btn btn-warning float-end me-2"
+          onClick={updateCourse} id="wd-update-course-click">
+          Update
+        </button>
+      </h5><br />
+      <input value={course.name} className="form-control mb-2"
+        onChange={(e) => setCourse({ ...course, name: e.target.value })} />
+      <textarea value={course.description} className="form-control"
+        onChange={(e) => setCourse({ ...course, description: e.target.value })} />
+      <hr />
       <h2 id="wd-dashboard-published">Published Courses ({courses.length})</h2> <hr />
       <div id="wd-dashboard-courses" className="row">
         <div className="row row-cols-1 row-cols-md-5 g-4">
@@ -12,7 +57,7 @@ export default function Dashboard() {
             <div className="wd-dashboard-course col" style={{ width: "300px" }}>
               <Link to={`/Kanbas/Courses/${course._id}/Home`} className="text-decoration-none" >
                 <div className="card rounded-3 overflow-hidden">
-                  <img src={`/images/${course._id}.png`} height="{160}" />
+                  <img src={course.image ? course.image : `/images/react.webp`} style={{ height: "160px", objectFit: "cover", width: "100%" }} />
                   <div className="card-body">
                     <span className="wd-dashboard-course-link"
                       style={{ textDecoration: "none", color: "navy", fontWeight: "bold" }} >
@@ -22,6 +67,22 @@ export default function Dashboard() {
                       {course.description}
                     </p>
                     <Link to={`/Kanbas/Courses/${course._id}/Home`} className="btn btn-primary">Go</Link>
+                    <button onClick={(event) => {
+                      event.preventDefault();
+                      deleteCourse(course._id);
+                    }} className="btn btn-danger float-end"
+                      id="wd-delete-course-click">
+                      Delete
+                    </button>
+                    <button id="wd-edit-course-click"
+                      onClick={(event) => {
+                        event.preventDefault();
+                        setCourse(course);
+                      }}
+                      className="btn btn-warning me-2 float-end" >
+                      Edit
+                    </button>
+
                   </div>
                 </div>
               </Link>
